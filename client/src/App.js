12c3b5aad24941cfeac5c9b11fe1cc8c80d87c45
anuannasharon <anuannasharon@gmail.com>
2021-10-03 19:27:40 +0530

    router change

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import {
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/hackideas">
         <Switch>
           <Route path="/home">
             <Home/>
